3205f6515e73228d65ad4a1059e552fc3c771dec
leon-wee <leonwee27@gmail.com>
2017-11-08 23:46:48 -0500

    Used ejs files

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ import apiRouter from './api';
 import express from 'express';
 const server = express();
 
+//magic line
+server.set('view engine', 'ejs');
+
 server.get('/', (req, res) => {
-    res.send('Hello Express');
+    res.render('index');
 });
 
 server.use('/api', apiRouter);
