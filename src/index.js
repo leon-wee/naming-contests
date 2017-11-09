309d575b4bed542a397f8a90be6e89538b1540d1
leon-wee <leonwee27@gmail.com>
2017-11-09 17:11:33 -0500

    modularized html components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
-const App = (props) => {
-    return (
-        <h2 className="text-center">
-            {props.headerMessage}
-        </h2>
-    );
-};
-
-App.propTypes = {
-    headerMessage: PropTypes.string.isRequired
-};
-
-App.defaultProps = {
-    headerMessage: 'Hello!!'
-};
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
