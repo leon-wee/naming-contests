8b959f013d24df75b122c4cdbd679afd8d91108c
leon-wee <leonwee27@gmail.com>
2017-11-08 23:38:56 -0500

    Played around with api router

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import config from './config';
-import fs from 'fs';
+import apiRouter from './api';
+
 import express from 'express';
 const server = express();
 
@@ -7,11 +8,8 @@ server.get('/', (req, res) => {
     res.send('Hello Express');
 });
 
-server.get('/about.html', (req, res) => {
-    fs.readFile('./about.html', (err, data) => {
-        res.send(data.toString());
-    });
-});
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
     console.info('Express listening on port: ', config.port);
