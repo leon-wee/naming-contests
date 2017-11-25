f2f1534cc9423f83490f01d81b1e54794394b359
leon-wee <leonwee27@gmail.com>
2017-11-24 23:05:02 -0500

    Make sure to return for ContestList

---
+++ src/components/ContestList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ContestPreview from './ContestPreview';
+
+const ContestList = ({ contests }) => {
+    return  (
+        <div className="ContestList">
+            {contests.map(contest =>
+                <ContestPreview key={contest.id} {...contest} />
+            )}
+        </div>
+    );
+};
+
+ContestList.propTypes = {
+    contests: PropTypes.array
+};
+
+export default ContestList;
