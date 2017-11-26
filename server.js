391ac973f72d4d3920b57237ea20976682eb8e6a
leon-wee <leonwee27@gmail.com>
2017-11-25 19:41:39 -0500

    Implemented post for adding names

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ import config from './config';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 import express from 'express';
 const server = express();
-
+server.use(bodyParser.json());
 //using sass middleware
 server.use(sassMiddleware({
     src: path.join(__dirname, 'sass'),
